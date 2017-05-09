2eea80e57cbeac81632455a817ffee338332c275
Woshiajuana <zhigang.chen@owulia.com>
2017-05-09 18:19:22 +0800

    浪笔头接口开发

---
+++ api/interface.js
@@ -0,0 +1,202 @@
+/**
+ * Created by Administrator on 2017/4/24.
+ * 接口工具库
+ */
+
+/**引入分页查询工具库*/
+const dbHelper = require('./page-query');
+
+/**引入token工具*/
+const jwt = require('jsonwebtoken');
+
+/**引入数据库工具库*/
+const models = require('../database/db');
+const users = models.user;
+const articles = models.article;
+
+/**引入express包*/
+const express = require('express');
+
+/**创建路由*/
+const router = express.Router();
+
+/**验证token的中间键*/
+const check_api_token = require('./check_api_token');
+
+/**创建接口*/
+/**用户登录*/
+router.post('/thepalestink/login',(req,res) => {
+    /**这里的req.body能够使用就在index.js中引入了const bodyParser = require('body-parser')*/
+    if(!req.query.user_name) {
+        res.json({status: 0, msg: '请输入帐号'});
+        return;
+    }
+    if(!req.query.user_password) {
+        res.json({status: 0, msg: '请输入密码'});
+        return;
+    }
+    let user = {
+        user_name: req.query.user_name,
+        user_password: req.query.user_password
+    };
+    users.find(user, function(err, doc){
+        if(doc){
+            /**创建token*/
+            let token = jwt.sign(user, 'app.get(superSecret)', {
+                expiresIn: 60*60*24 /**设置过期时间*/
+            });
+            res.json({
+                status: 1,
+                msg: '登陆成功',
+                data: {
+                    token,
+                    user: {
+                        _id: doc[0]._id,
+                        user_name: doc[0].user_name
+                    }
+                }
+            });
+        }else{
+            res.json({
+                status: 0,
+                msg: '帐号或密码不正确'
+            });
+        }
+    });
+});
+/**验证用户名是否已注册*/
+router.get('/thepalestink/checkUserName',(req,res) => {
+    let user = {
+        user_name: req.query.user_name
+    };
+    users.count(user, function(err, doc){
+        if(doc){
+            res.json({
+                status: 0,
+                msg: '帐号已存在'
+            });
+        }else{
+            res.json({
+                status: 1,
+                msg: '帐号可以注册'
+            });
+        }
+    });
+});
+/**用户注册*/
+router.post('/thepalestink/register',(req,res) => {
+    if(!req.query.user_name) {
+        res.json({status: 0, msg: '请输入帐号'});
+        return;
+    }
+    if(!req.query.user_password) {
+        res.json({status: 0, msg: '请输入密码'});
+        return;
+    }
+    if(req.query.user_password != req.query.user_too_password) {
+        res.json({status: 0, msg: '两次密码不一致'});
+        return;
+    }
+    let user = {
+        user_name: req.query.user_name,
+        user_password: req.query.user_password
+    };
+    users.create(user, function(err, doc){
+        if(err){
+            res.json({status: 0, msg: '注册失败'});
+        }else {
+            res.json({status: 1, msg: '注册成功'});
+        }
+    });
+});
+
+
+/**获取文章数据*/
+router.get('/ajuan/fetchArticle',(req,res) => {
+    let article_type = req.query.tab == 'all' ? '' : req.query.tab;
+    let page = +req.query.page_num || 1;
+    let rows = +req.query.page_size || 12;
+    let key_word = req.query.key_word;
+    let query = {};
+    if(article_type) query.article_type = article_type;
+    if(key_word) query.article_title =  eval("/"+key_word+"/ig");
+    dbHelper.pageQuery(page, rows, articles, '', query, {}, (error, $page) => {
+        if(error){
+            res.json({status: 0, msg: '获取信息失败'});
+        }else{
+            res.json({
+                status:1,
+                data: $page.results,
+                article_total: $page.total,
+                page_count: Math.ceil($page.pageCount)
+            });
+        }
+    });
+});
+
+/**根据文章_id获取文章数据*/
+router.get('/ajuan/fetchDetail',(req,res) => {
+    let _id = req.query.article_id;
+    articles.find({
+        _id: _id
+    },(err,doc) => {
+        if(err){
+            res.json({status:0,msg:'获取信息失败'})
+        }else{
+            res.json({status:1,data:doc,msg:'获取信息成功'});
+        }
+    });
+});
+
+/**后台获取文章数据*/
+router.get('/ajuan_backstage/fetchArticle',check_api_token,(req,res) => {
+    let article_type = req.query.tab == 'all' ? '' : req.query.tab;
+    let page = +req.query.page_num || 1;
+    let rows = +req.query.page_size || 12;
+    let key_word = req.query.key_word;
+    let query = {};
+    if(article_type) query.article_type = article_type;
+    if(key_word) query.article_title =  eval("/"+key_word+"/ig");
+    dbHelper.pageQuery(page, rows, articles, '', query, {}, (error, $page) => {
+        if(error){
+            res.json({status: 0, msg: '获取信息失败'});
+        }else{
+            res.json({
+                status:1,
+                data: $page.results,
+                article_total: $page.total,
+                page_count: Math.ceil($page.pageCount)
+            });
+        }
+    });
+});
+
+/**创建文章*/
+router.get('/ajuan_backstage/insertArticle',check_api_token,(req,res) => {
+    if(!req.query.article){
+        res.json({status: 0, msg: '请把信息填写完整'});
+        return;
+    }
+    let article = JSON.parse(req.query.article);
+    articles.create(article,(err, doc) => {
+        if(err){
+            res.json({status: 0, msg: '提交文章失败'});
+        }else {
+            res.json({status: 1, msg: '提交文章成功'});
+        }
+    });
+});
+
+/**删除文章*/
+router.get('/ajuan_backstage/removeArticle',check_api_token,(req,res) => {
+    let article_id = req.query.article_id;
+    articles.remove({_id:article_id},(err, doc) => {
+        if(err){
+            res.json({status: 0, msg: '删除文章失败'});
+        }else {
+            res.json({status: 1, msg: '删除文章成功'});
+        }
+    })
+});
+
+module.exports = router;
---
+++ database/db.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Administrator on 2017/4/24.
+ * 链接数据库的db工具库
+ */
+
+/**引入包mongoose*/
+const mongoose = require('mongoose');
+
+/**连接数据库*/
+const db = mongoose.connect('mongodb://localhost/thepalestink');
+
+/**创建模型*/
+const Schema = mongoose.Schema;
+
+/**
+ * 定义了一个新的模型，
+ * 对mongodb数据库article表的映射
+ * 但是此模式还未和article集合有关联
+ * */
+const articleScheMa = new Schema({
+    article_title: String,
+    article_type: String,
+    article_time: String,
+    article_content: String
+},{
+    versionKey: false
+});
+
+/**
+ * 定义了一个新的模型，
+ * 对mongodb数据库users表的映射
+ * 但是此模式还未和users集合有关联
+ * */
+const userScheMa = new Schema({
+    user_name: String,
+    user_password: String
+},{
+    versionKey: false
+});
+
+
+const Models = {
+    /**与users集合关联*/
+    article: db.model('articles',articleScheMa),
+    user: db.model('users',userScheMa)
+};
+
+module.exports = Models;
